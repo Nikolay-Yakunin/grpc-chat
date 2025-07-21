32ed0cf6a7b7823e3b35128ce411509137df8b28
Nikolay-Yakunin <akuninn52@gmail.com>
2025-07-21 15:50:18 +0700

    implement config

--- pkg/token/jwt.go
+++ pkg/token/jwt.go
@@ -1,13 +1,16 @@
 package token
 
 import (
-	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+
+    "github.com/Nikolay-Yakunin/grpc-chat/pkg/config"
 )
 
 func GenToken(name string) (string, error) {
+    env := config.NewENV()
+
     claims := jwt.MapClaims{
         "sub": name,
         "exp": time.Now().Add(24 * time.Hour).Unix(),
@@ -15,7 +18,7 @@ func GenToken(name string) (string, error) {
     }
 
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    secretKey := []byte(os.Getenv("JWT_SECRET"))
+    secretKey := []byte(env.JWT_SECRET)
 
     return token.SignedString(secretKey)
 }
